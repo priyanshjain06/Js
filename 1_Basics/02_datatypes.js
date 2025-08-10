94c9c62129b5b8fd1f877d921e175b21b6687270
Priyansh Jain <priyansh01jain@gmail.com>
2025-08-10 11:26:34 +0530

    Updated the code !!

--- 1_Basics/02_datatypes.js
+++ 1_Basics/02_datatypes.js
@@ -1,30 +1,23 @@
-
-"use strict"; //REVIEW  treat all JS code as newer version
+"use strict"; //REVIEW  makes the silent errors to throw error to debug them
 
 // alert( 3 + 3) // we are using nodejs, not browser
 
-console.log(3 
-    +
-     3) // code readability should be high
+console.log(3 + 3); // code readability should be high
+console.log("Hitesh");
 
-console.log("Hitesh")
-
-
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
 let state;
 
 // number => 2 to power 53
 // bigint
-// string => ""
+// string => ""   
 // boolean => true/false
-// null => standalone value
-// undefined => 
-// 
-symbol => unique
-
+// null => standalone value 
+// undefined =>
+//(symbol) => unique;
 // object
 
 console.log(typeof undefined); // undefined
-console.log(typeof null); //REVIEW  object 
+console.log(typeof null); //REVIEW  object
