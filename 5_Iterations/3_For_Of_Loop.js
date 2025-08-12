420b2239e4441a014a147410b8f3fb8b8416f050
Priyansh Jain <priyansh01jain@gmail.com>
2025-08-12 10:06:49 +0530

    Updated the code !

--- 5_Iterations/3_For_Of_Loop.js
+++ 5_Iterations/3_For_Of_Loop.js
@@ -1,51 +1,53 @@
+//REVIEW use for array string map  , not for objects
+
 // for of synatx
 // for (const iterator of object) {
-    
+
 // }
 
 // ["", "", ""]
 // [{}, {}, {}]
 
-const arr = [1, 2, 3, 4, 5]
+const arr = [1, 2, 3, 4, 5];
 
 for (const num of arr) {
-    //console.log(num);
+  //console.log(num);
 }
 
-const greetings = "Hello world!"
+const greetings = "Hello world!";
 for (const greet of greetings) {
-    //console.log(`Each char is ${greet}`)
-    // H e l l o  w o r l d !
+  //console.log(`Each char is ${greet}`)
+  // //REVIEW $ and  `` is used
+  // H e l l o  w o r l d !
 }
 
- // for map
-const map = new Map()
-map.set('IN', "India")
-map.set('USA', "United States of America")
-map.set('Fr', "France")
-map.set('IN', "India")
+// for map
+const map = new Map();
+map.set("IN", "India");
+map.set("USA", "United States of America");
+map.set("Fr", "France");
+map.set("IN", "India");
 
 // console.log(map);
 for (const key of map) {
-    console.log(key);
+  console.log(key);
 }
 
-
 for (const [key, value] of map) {
-    console.log(key, ':-', value);
+  console.log(key, ":-", value);
 }
 
 const myObject = {
-    game1: 'NFS',
-    game2: 'Spiderman'
-}
+  game1: "NFS",
+  game2: "Spiderman",
+};
 
 // it also gives error
 // for (const key of myObject) {
 //     console.log(key);
 // }
 
-//REVIEW  OBJECTS are not iterable by for of loop  so it gives error 
+//REVIEW  OBJECTS are not iterable by for of loop  so it gives error
 // for (const [key, value] of myObject) {
 //     console.log(key, ':-', value);}
 
@@ -59,9 +61,8 @@ const myObject = {
 //   console.log(item); // ❌ TypeError: myObject is not iterable
 // }
 
-
 // REVIEW Or use Object.entries() with for...of
 
 for (const [key, value] of Object.entries(myObject)) {
   console.log(key, ":", value);
-}   
+}
