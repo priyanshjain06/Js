18abf7c310d52d1c59d738059c763b88ae697d62
Priyansh Jain <priyansh01jain@gmail.com>
2025-12-18 10:32:11 +0530

    updated

--- 1_Basics/05_comparision.js
+++ 1_Basics/05_comparision.js
@@ -32,7 +32,7 @@ console.log(null >= 0); //REVIEW true  bocz null gets converted into 0
 console.log(undefined == 0); // false
 console.log(undefined > 0); // false
 console.log(undefined < 0); // false
-    
+
 //In JavaScript, === is the strict equality operator. It compares two values for equality and returns true if they are equal and of the same type.
 
 console.log("2" === 2); // false
