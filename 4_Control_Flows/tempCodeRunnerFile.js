16b196038448c7b2d6a4730c019fa903e9da0448
Priyansh Jain <priyansh01jain@gmail.com>
2025-12-18 14:38:48 +0530

    updated

---
+++ 4_Control_Flows/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+const score = 200
+
+if (score > 100) {
+    let power = "fly"
+    console.log(`User power: ${power}`);
+//REVIEW -  no this here since no function  or object
+}
+
+console.log(`User power: ${power}`);
