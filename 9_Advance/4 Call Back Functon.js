65faedd27f63d27749ce9b0803457a2193f05e3e
Priyansh Jain <priyansh01jain@gmail.com>
2025-12-19 17:23:40 +0530

    updated !

--- 9_Advance/4 Call Back Functon.js
+++ 9_Advance/4 Call Back Functon.js
@@ -1,12 +1,13 @@
-//REVIEW -  function passed as argument to another function !! and is called back
+//REVIEW -  If you PASS a function as an  argument to another function call  and someone else CALLS it later → it’s a callback.
+
 function greet(name) {
   console.log("Hello, " + name + "!");
 }
 
 function delayedGreeting(name, callback) {
   setTimeout(() => {
-    callback(name);
-  }, 2000); // 2000 milliseconds = 2 seconds
+    callback(name); //callback(name) calls the function passed as an argument and passes name to it.
+  }, 2000);
 }
 
 delayedGreeting("Alice", greet);
--- 9_Advance/7 Promise Chaining.js
+++ 9_Advance/7 Promise Chaining.js
@@ -1,48 +1,60 @@
+//REVIEW -  Promise chaining means linking multiple .then() calls together, where each .then() waits for the previous one to finish and uses its result.
+
+// 📌 In one line:
+// Output of one .then() becomes input of the next .then().
+//.then() always returns a new Promise
+
+//REVIEW -  we do promise chaining to avoid call back hell  !
+
 let stocks = {
-    Fruits: ["strawberry", "grapes", "banana", "apple"],
-    liquid: ["water", "ice"],
-    holder: ["cone", "cup", "stick"],
-    toppings: ["chocolate", "peanuts"],
-  };
-  
-  let is_shop_open = true;
-  
-  let order = (time, work) => {
-    return new Promise((resolve, reject) => {
-      if (is_shop_open) {
-        setTimeout(() => {
-          resolve(work());
-        }, time);
-      } else {
-        reject(console.log("Our shop is closed"));
-      }
-    });
-  };
-  
-  order(2000, () => {
-    console.log(`${stocks.Fruits[0]} was selected`);
+  Fruits: ["strawberry", "grapes", "banana", "apple"],
+  liquid: ["water", "ice"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "peanuts"],
+};
+
+let is_shop_open = true;
+
+let order = (time, work) => {
+  return new Promise((resolve, reject) => {
+    if (is_shop_open) {
+      setTimeout(() => {
+        resolve(work());
+      }, time);
+    } else {
+      reject(console.log("Our shop is closed"));
+    }
+  });
+};
+
+order(2000, () => {
+  console.log(`${stocks.Fruits[0]} was selected`);
+})
+  .then(() => {
+    return order(0, () => console.log("production has started"));
   })
-    .then(() => {
-      return order(0, () => console.log("production has started"));
-    })
-    .then(() => {
-      return order(2000, () => console.log("The fruit has been chopped"));
-    })
-    .then(() => {
-      return order(1000, () => console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} Added`));
-    })
-    .then(() => {
-      return order(1000, () => console.log("start the machine"));
-    })
-    .then(() => {
-      return order(2000, () => console.log(`Ice cream placed on ${stocks.holder[1]}`));
-    })
-    .then(() => {
-      return order(3000, () => console.log(`${stocks.toppings[0]} as toppings`));
-    })
-    .then(() => {
-      return order(2000, () => console.log("serve Ice cream"));
-    })
-    .catch((error) => {
-      console.log("Production error:", error);
-    }); 
+  .then(() => {
+    return order(2000, () => console.log("The fruit has been chopped"));
+  })
+  .then(() => {
+    return order(1000, () =>
+      console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} Added`)
+    );
+  })
+  .then(() => {
+    return order(1000, () => console.log("start the machine"));
+  })
+  .then(() => {
+    return order(2000, () =>
+      console.log(`Ice cream placed on ${stocks.holder[1]}`)
+    );
+  })
+  .then(() => {
+    return order(3000, () => console.log(`${stocks.toppings[0]} as toppings`));
+  })
+  .then(() => {
+    return order(2000, () => console.log("serve Ice cream"));
+  })
+  .catch((error) => {
+    console.log("Production error:", error);
+  });
