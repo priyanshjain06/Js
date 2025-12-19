720b078fd8ca1a1f20315678e98c0c629a22d484
Priyansh Jain <priyansh01jain@gmail.com>
2025-12-19 18:36:30 +0530

    updated lexcal 13 folder !!

--- 11 Lexical Scoping and Closures/1 Lexical Scoping.js
+++ 11 Lexical Scoping and Closures/1 Lexical Scoping.js
@@ -1,12 +1,21 @@
-function outer()
- {
-    let name = "Mozilla";
-        function inner()
-         {
-            console.log(name);
-         }
-    inner();
-  } 
-  
-  outer();
-  
+//REVIEW concept : Global Scope
+//  └── a = 10
+//      └── outer()
+//           └── b = 20
+//               └── inner()
+
+let a = 10;
+
+function outer() {
+  let b = 20;
+  //REVIEW -  can access a ( global)
+
+  function inner() {
+    console.log(a); // REVIEW can access a  (global)
+    console.log(b); //REVIEW can access parent
+  }
+
+  inner();
+}
+
+outer();
--- 11 Lexical Scoping and Closures/2 Closure.js
+++ 11 Lexical Scoping and Closures/2 Closure.js
@@ -1,16 +1,15 @@
-function outer()
- {
-    let name = "Mozilla";
-        function inner()
-         {
-            console.log(name);
-         }
-    return inner;
-  } 
-  
-  const call = outer();
-  call();
-  
+function outer() {
+  let name = "Mozilla";
+  function inner() {
+    console.log(name);
+  }
+  return inner; //REVIEW - 
+}
+
+//   outer();
+//   console.log(`outer function is finished `);
+const call = outer();
+call();
+
 //   closure gives you access to an outer function's scope from an inner function.
 //  In JavaScript, closures are created every time a function is created, at function creation time.
-
