03349f8ca6b1264255a979c509a07d0bf660fc77
Priyansh Jain <priyansh01jain@gmail.com>
2026-01-06 11:30:45 +0530

    deleted  temp file

--- 9_Advance/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-const p = Promise.all([1, 2, 3]);
-const p2 = Promise.all([1, 2, 3, Promise.resolve(444)]);
-const p3 = Promise.all([1, 2, 3, Promise.reject(555)]);
-
-setTimeout(() => {
-  console.log(p);
-  console.log(p2);
-  console.log(p3);
-});
