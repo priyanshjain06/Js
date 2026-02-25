05bbf85cb153939db14101043a3a8db48a0e2eea
Priyansh Jain <priyansh01jain@gmail.com>
2026-02-25 09:48:51 +0530

    added commnets

--- 11 Lexical Scoping and Closures/2 Closure.js
+++ 11 Lexical Scoping and Closures/2 Closure.js
@@ -10,3 +10,5 @@ const call = outer();
 call();
 
 //REVIEW - A closure is created when a function remembers and can access variables from its outer (lexical) scope, even after the outer function has finished executing.
+
+//REVIEW -  used for data encapsulation
